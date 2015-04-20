b13eb4f17ad10862025c0a676ba491c727af470b
Julian van Riet <vanrietjulian@gmail.com>
2015-04-20 10:30:44 -0700

    Quiz Code

---
+++ main.js
@@ -0,0 +1,9 @@
+$('.dan').click(function(){
+	alert('Hey you clicked on me');
+	//console.log("clicked dan");
+});
+
+$('#iwao').click(function(){
+	alert("Hey you clicked on me again");
+	//console.log("clicked iwao");
+});
